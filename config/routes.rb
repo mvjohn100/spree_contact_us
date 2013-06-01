9a6601f9cbf4472b1df5919d5c1d1adc4f38c91c
John <john@klairvoyant.com>
2013-06-01 14:21:55 +0530

    june 1  2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Spree::Core::Engine.routes.draw do
   resources :contacts,
     :controller => 'contact_us/contacts',
     :only       => [:new, :create]
-  match '/store/contact-us' => '/store/contact_us/contacts#new', :as => :contact_us
+  match 'contact-us' => 'contact_us/contacts#new', :as => :contact_us
 end
